b3d8719772a7c3aede5852fd01bc908bcad7b6e4
vimalprasathr <44838054+vimalprasathr@users.noreply.github.com>
2019-03-08 15:44:16 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,11 @@
 /* This is a simple Java program. 
 FileName : "HelloWorld.java". */
-class HelloWorld 
+class HelloVimal 
 { 
 	// Your program begins with a call to main(). 
 	// Prints "Hello, World" to the terminal window. 
 	public static void main(String args[]) 
 	{ 
-		System.out.println("Hello, World"); 
+		System.out.println("Hello, Vimal"); 
 	} 
 } 
